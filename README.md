5f3e0677575b7a9c1c1bf40a977cdc6c7a8b0b02
Mordi Sacks <mordisacks@gmail.com>
2016-06-12 17:20:03 +0300

    initial commit...

---
+++ README.md
@@ -0,0 +1,19 @@
+# I18n
+I18n Wrapper for Laravel 5.
+
+# Installation
+```
+composer require mordisacks/laravel-i18n
+```
+
+After updating composer, add the service provider to the providers array in config/app.php
+```
+MordiSacks\LaravelI18n\I18nServiceProvider::class,
+```
+
+# Usage
+Set your default locale in .env file
+```
+I18N=he_IL
+```
+More coming soon
---
+++ src/I18nServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+namespace MordiSacks\LaravelI18n;
+
+use Illuminate\Support\ServiceProvider;
+use MordiSacks\I18n\I18n;
+
+class I18nServiceProvider extends ServiceProvider
+{
+
+	public function boot()
+	{
+		if(!file_exists(resource_path('i18n')))
+		{
+			mkdir(resource_path('i18n'), 0755);
+		}
+
+		I18n::setDir(resource_path('i18n'));
+
+		I18n::setLocale(env('I18N'));
+
+		\Blade::directive('lang', function ($expression)
+		{
+			return "<?php echo __{$expression}; ?>";
+		});
+	}
+
+	public function register()
+	{
+
+	}
+}
